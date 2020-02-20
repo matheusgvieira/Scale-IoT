ccac9e98954fbcd0155bd1e2e5964476991cda3c
Matheus Gois <matheusmaxi9.0@gmail.com>
2020-02-20 12:56:57 -0300

    Atualização 20/02

--- software/balanca_hx.ino
+++ software/balanca_hx.ino
@@ -1,93 +1,89 @@
 
 /*
- *  Autor: Matheus Gois Vieira
- *  Empresa: ContaHub
- *  Sketch:  Módulo para Arduino HX711
- *  Data: 27/01/2020
+    Autor: Matheus Gois Vieira
+    Empresa: ContaHub
+    Sketch:  Módulo para Arduino HX711
+    Data: 20/02/2020
 */
-
 
 // INCLUSÃO DE BIBLIOTECAS
 #include <HX711.h>
-#include <PushButton.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-
-
 // DEFINIÇÕES DE PINOS
 #define pinDT  2
 #define pinSCK  3
-#define pinBotao 4
-
-// DEFINIÇÕES
-#define pesoMin 0.010
-#define pesoMax 30.0
-
-#define escala 0.0f
 
 // INSTANCIANDO OBJETOS
 HX711 scale;
-PushButton botao(pinBotao);
-SoftwareSerial mySerial(10,11);
-LiquidCrystal_I2C lcd(0x27,16,2);
+SoftwareSerial mySerial(10, 11);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// DECLARAÇÃO DE VARIÁVEIS  
-float medida=0;
+// DECLARAÇÃO DE VARIÁVEIS
+float medida = 0;
 char str[80];
+int aux = 0;
 
 void setup() {
   Serial.begin(57600);
   mySerial.begin(115200);
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.init();
 
+
   scale.begin(pinDT, pinSCK); // CONFIGURANDO OS PINOS DA BALANÇA
-  scale.set_scale(100815); // ENVIANDO O VALOR DA ESCALA CALIBRADO
+  scale.set_scale(20702); // LIMPANDO O VALOR DA ESCALA
 
   delay(2000);
   scale.tare(); // ZERANDO A BALANÇA PARA DESCONSIDERAR A MASSA DA ESTRUTURA
-  Serial.println("Setup Finalizado!");
+  Serial.println("Iniciando Balança");
 
   lcd.setBacklight(HIGH);
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("ContaHUB");
 }
 
 void loop() {
-  botao.button_loop(); // LEITURA DO BOTAO
 
-  if(botao.pressed()){ // QUANDO PRESSIONAR O BOTÃO
-    
-    scale.power_up(); // LIGANDO O SENSOR
-    
-    medida = scale.get_units(5); // SALVANDO NA VARIAVEL O VALOR DA MÉDIA DE 5 MEDIDAS
-    
-    if (medida <= pesoMin ){ // CONFERE SE A MASSA ESTÁ NA FAIXA VÁLIDA
-      scale.tare(); // ZERA A BALANÇA CASO A MASSA SEJA MENOR QUE O VALOR MIN
-      medida = 0;
-      Serial.println("Tara Configurada!");
-    } else if ( medida >= pesoMax ){
-      scale.tare(); // ZERA A BALANÇA CASO A MASSA SEJA MAIOR QUE O VALOR MÁX
-      medida = 0;
-      Serial.println("Tara Configurada!");
-    } else {
-      Serial.println(medida,3);
-   
-      dtostrf(medida,3,3,str);
-      Serial.println("-------------------------");
-      Serial.println(str);
-      Serial.println("-------------------------");
-      lcd.setCursor(0,1);
-      lcd.print(str);
-      mySerial.write(str);
-      Serial.println("Valor enviado");
-    }
+  medida = scale.get_units(5); // SALVANDO NA VARIAVEL O VALOR DA MÉDIA DE 5 MEDIDAS
+  Serial.println(medida, 3); // ENVIANDO PARA MONITOR SERIAL A MEDIDA COM 3 CASAS DECIMAIS
 
-    scale.power_down(); // DESLIGANDO O SENSOR
-  }
   
-}
 
-// IMPLEMENTO DE FUNÇÕES
+  if (medida < 0.06) {
+    medida = 0;
+    aux == 0;
+
+    // Send to serial and hardware
+    Serial.println(medida, 3);      // Print monitor serial
+    dtostrf(medida, 3, 3, str);     // Int to String
+    lcd.setCursor(0, 1);            // Position in the display
+    lcd.print(str);                 // Print in the display
+    mySerial.write(str);            // Enviando valor via serial TX/RX
+
+    while ((medida == 0) && (aux == 0)) {
+      medida = scale.get_units(10); // SALVANDO NA VARIAVEL O VALOR DA MÉDIA DE 10 MEDIDAS
+      aux = (medida < 0.06) ? 0 : 1;
+    }
+  }
+
+  else {
+
+    // Send to serial and hardware
+    Serial.println(medida, 3);      // Print monitor serial
+    dtostrf(medida, 3, 3, str);     // Int to String
+    lcd.setCursor(0, 1);            // Position in the display
+    lcd.print(str);                 // Print in the display
+    mySerial.write(str);            // Sendo value to TX/RX
+    aux = medida;
+
+    while ( (medida == aux) || (((aux - 0.06) < medida) && (medida < (aux + 0.06))) ) {
+      aux = scale.get_units(10); // SALVANDO NA VARIAVEL O VALOR DA MÉDIA DE 10 MEDIDAS
+    }
+  }
+  scale.power_down(); // DESLIGANDO O SENSOR
+  delay(1000); // AGUARDA 5 SEGUNDOS
+  scale.power_up(); // LIGANDO O SENSOR
+}
---
+++ software/balanca_hx_button.ino
@@ -0,0 +1,93 @@
+
+/*
+ *  Autor: Matheus Gois Vieira
+ *  Empresa: ContaHub
+ *  Sketch:  Módulo para Arduino HX711
+ *  Data: 27/01/2020
+*/
+
+
+// INCLUSÃO DE BIBLIOTECAS
+#include <HX711.h>
+#include <PushButton.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+
+
+// DEFINIÇÕES DE PINOS
+#define pinDT  2
+#define pinSCK  3
+#define pinBotao 4
+
+// DEFINIÇÕES
+#define pesoMin 0.010
+#define pesoMax 30.0
+
+#define escala 0.0f
+
+// INSTANCIANDO OBJETOS
+HX711 scale;
+PushButton botao(pinBotao);
+SoftwareSerial mySerial(10,11);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+// DECLARAÇÃO DE VARIÁVEIS  
+float medida=0;
+char str[80];
+
+void setup() {
+  Serial.begin(57600);
+  mySerial.begin(115200);
+  lcd.begin(16,2);
+  lcd.init();
+
+  scale.begin(pinDT, pinSCK); // CONFIGURANDO OS PINOS DA BALANÇA
+  scale.set_scale(20702); // ENVIANDO O VALOR DA ESCALA CALIBRADO
+
+  delay(2000);
+  scale.tare(); // ZERANDO A BALANÇA PARA DESCONSIDERAR A MASSA DA ESTRUTURA
+  Serial.println("Setup Finalizado!");
+
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("ContaHUB");
+}
+
+void loop() {
+  botao.button_loop(); // LEITURA DO BOTAO
+
+  if(botao.pressed()){ // QUANDO PRESSIONAR O BOTÃO
+    
+    scale.power_up(); // LIGANDO O SENSOR
+    
+    medida = scale.get_units(5); // SALVANDO NA VARIAVEL O VALOR DA MÉDIA DE 5 MEDIDAS
+    
+    if (medida <= pesoMin ){ // CONFERE SE A MASSA ESTÁ NA FAIXA VÁLIDA
+      scale.tare(); // ZERA A BALANÇA CASO A MASSA SEJA MENOR QUE O VALOR MIN
+      medida = 0;
+      Serial.println("Tara Configurada!");
+    } else if ( medida >= pesoMax ){
+      scale.tare(); // ZERA A BALANÇA CASO A MASSA SEJA MAIOR QUE O VALOR MÁX
+      medida = 0;
+      Serial.println("Tara Configurada!");
+    } else {
+      Serial.println(medida,3);
+   
+      dtostrf(medida,3,3,str);
+      Serial.println("-------------------------");
+      Serial.println(str);
+      Serial.println("-------------------------");
+      lcd.setCursor(0,1);
+      lcd.print(str);
+      mySerial.write(str);
+      Serial.println("Valor enviado");
+    }
+
+    scale.power_down(); // DESLIGANDO O SENSOR
+  }
+  
+}
+
+// IMPLEMENTO DE FUNÇÕES
